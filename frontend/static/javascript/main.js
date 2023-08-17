3d695c9050dadd4d19d5f387995d84261ceba2fc
Vaksoa <kewin.talaga@gmail.com>
2023-08-17 20:01:41 +0300

    Complete rewrite to websockets

--- frontend/static/javascript/main.js
+++ frontend/static/javascript/main.js
@@ -1,26 +1,15 @@
-const API_URL = 'http://localhost:80';
+const API_URL = 'ws://localhost:80/ws';
 const USERNAME_PROMPT = "Please enter your name before proceeding. We need that so we know who you are.";
 const USERNAME_ERROR_PROMPT = "An error occurred. Maybe it is because this name exists. If so, please choose another one. Please enter your name.";
 
 let lastMessageId = 0;
 let lastUserId = 0;
 let Username;
+let socket;
 
 window.addEventListener('load', async () => {
     await login();
     setupEventListeners();
-    loadMessages();
-    loadActiveUsers();
-    setInterval(loadMessages, 1000);
-    setInterval(loadActiveUsers, 1000);
-});
-
-window.addEventListener('beforeunload', () => {
-    fetch(`${API_URL}/leave/${Username}`, {
-        method: 'POST', headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
 });
 
 async function login() {
@@ -31,22 +20,32 @@ async function login() {
         inputname = sanitizeInput(inputname);
 
         try {
-            const response = await fetch(`${API_URL}/join/${inputname}`, {
-                method: 'POST', headers: {
-                    'Access-Control-Allow-Origin': '*'
+            Username = inputname;
+            socket = new WebSocket(`${API_URL}/${Username}`);
+            socket.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                if (data.type === 'message') {
+                    displayMessage(data.message);
+                } else if (data.type === 'join') {
+                    displaySystemMessage(`${data.name} has joined the chat.`);
+                } else if (data.type === 'leave') {
+                    displaySystemMessage(`${data.name} has left the chat.`);
+                } else if (data.type === 'active_users') {
+                    displayActiveUsers(data.users);
                 }
-            });
-
-            if (response.ok) {
-                alert("Successfully logged in! Enjoy.")
-                const usersDiv = document.querySelector('.user-list');
-                Username = inputname;
-                usersDiv.innerHTML = ""
-            } else if (response.status === 429) {
-                alert("You have exceeded the rate limit. Please wait before trying again.");
-            } else {
-                message = USERNAME_ERROR_PROMPT;
             }
+            socket.onopen = (event) => {
+                alert("Successfully logged in! Enjoy.")
+            }
+            socket.onclose = (event) => {
+                if (event.code === 4000) {
+                    alert("The username is invalid or already in use. Please choose another one.");
+                    window.location.reload();
+                }
+            }
+            window.addEventListener('beforeunload', () => {
+                socket.close();
+            });
         } catch (error) {
             alert("An error occurred while trying to join the chat. Please check your network connection and try again.");
         }
@@ -89,91 +88,67 @@ async function sendMessage() {
     };
 
     try {
-        const response = await fetch(`${API_URL}/messages`, {
-            method: 'POST', headers: {
-                'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
-            }, body: JSON.stringify(message)
-        });
-
-        if (response.ok) {
-            document.querySelector('.chatroom-textarea').value = '';
-            loadMessages();
-        } else if (response.status === 429) {
-            alert("You have exceeded the rate limit. Please wait before trying again.");
-        } else {
-            alert("An error occurred while trying to send your message. Please try again.");
-        }
+        socket.send(JSON.stringify({ type: 'message', message }));
+        document.querySelector('.chatroom-textarea').value = '';
     } catch (error) {
         alert("An error occurred while trying to send your message. Please check your network connection and try again.");
     }
 }
 
-async function loadMessages() {
-    const response = await fetch(`${API_URL}/messages`);
-    const messages = await response.json();
+function displayMessage(message) {
+
+    let messageContent;
+
+    if (/[_*~`#]/.test(message.content)) {
+        // The message content contains characters used in Markdown (like *, _, ~, `)
+        // Let's parse it as Markdown
+        messageContent = marked.parse(message.content);
+    } else {
+        // No special Markdown characters found in the message content,
+        // Let's display it as plain text
+        messageContent = sanitizeInput(message.content);
+    }
 
     const messagesDiv = document.querySelector('.main-contents');
 
-    for (const message of messages.slice(lastMessageId)) {
+    const messageElement = document.createElement('div');
 
-        let messageContent;
-
-        if (/[_*~`#]/.test(message.content)) {
-            // The message content contains characters used in Markdown (like *, _, ~, `)
-            // Let's parse it as Markdown
-            messageContent = marked.parse(message.content);
-        } else {
-            // No special Markdown characters found in the message content,
-            // Let's display it as plain text
-            messageContent = sanitizeInput(message.content);
-        }
-
-        const messageElement = document.createElement('div');
-
-        if (message.name === Username) {
-            messageElement.classList.add('chat-you');
-            messageElement.innerHTML = `<div class="chat-you-container">
-                                <div class="chat-you-message">
-                                    <span class="chatroom-text4">${messageContent}</span>
-                                </div>
-                            </div>`;
-
-            messagesDiv.appendChild(messageElement);
-            messagesDiv.scrollBy(0, messagesDiv.scrollHeight);
-
-            continue;
-        } else if (message.name === 'System') {
-            displaySystemMessage(messageContent);
-            continue;
-        }
-
-        messageElement.classList.add('chat-user');
-
-        messageElement.innerHTML = `<span class="chat-user-name">${message.name}</span>
-                            <div class="chat-user-container">
-                                <div class="chat-user-message">
-                                    <span class="chatroom-text5">${messageContent}</span>
-                                </div>
-                            </div>`;
+    if (message.name === Username) {
+        messageElement.classList.add('chat-you');
+        messageElement.innerHTML = `<div class="chat-you-container">
+                            <div class="chat-you-message">
+                                <span class="chatroom-text4">${messageContent}</span>
+                            </div>
+                        </div>`;
 
         messagesDiv.appendChild(messageElement);
-
         messagesDiv.scrollBy(0, messagesDiv.scrollHeight);
+
+        return;
+    } else if (message.name === 'System') {
+        displaySystemMessage(messageContent);
+        return;
     }
 
-    lastMessageId = messages.length;
+    messageElement.classList.add('chat-user');
+
+    messageElement.innerHTML = `<span class="chat-user-name">${message.name}</span>
+                        <div class="chat-user-container">
+                            <div class="chat-user-message">
+                                <span class="chatroom-text5">${messageContent}</span>
+                            </div>
+                        </div>`;
+
+    messagesDiv.appendChild(messageElement);
+
+    messagesDiv.scrollBy(0, messagesDiv.scrollHeight);
 }
 
-async function loadActiveUsers() {
-    const response = await fetch(`${API_URL}/active_users`);
-    const users = await response.json();
-
+function displayActiveUsers(users) {
     const usersDiv = document.querySelector('.user-list');
     usersDiv.innerHTML = '';
 
     for (const user of users) {
-        if (user === Username) continue;
-
         const userElement = document.createElement('div');
         userElement.classList.add('chatroom-user', 'user');
         userElement.innerHTML = `<div class="chatroom-text2">
